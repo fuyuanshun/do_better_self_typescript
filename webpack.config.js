c00a86a2218a635c6b6a7674bd5899a2572c8d6c
fuyuanshun <fuyuanshun521@gmail.com>
2024-08-11 20:33:14 +0800

    初始化提交

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path")
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry:"./src/index.ts",
+    output:{
+        //当前目录下的dist目录. 
+        path: path.resolve(__dirname, "dist"),
+        filename:'index.js'
+    },
+    module:{
+        rules:[
+            {
+                //正则表达式，对哪些文件生效
+                test:/\.css$/,
+                //loader执行顺序从下到上
+                //①通过css-loader，将css编译到js中
+                //②通过style-loader，创建style标签将css添加到js中
+                use:['style-loader','css-loader']
+            },
+            {
+                test:/\.ts$/,
+                use:['ts-loader']
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            //还需要在html中添加 <title><%= htmlWebpackPlugin.options.title %></title> 才可使标题生效
+            title:"主页",
+            //打包的文件名
+            filename:"index.html",
+            //模板
+            template:"./public/index.html",
+        })
+    ],
+    mode:"development"
+}
