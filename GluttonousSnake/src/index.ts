2eab075af99f3f50542d4d111204eb513b1b488e
fuyuanshun <fuyuanshun521@gmail.com>
2024-09-07 00:15:08 +0800

    排行榜功能

--- GluttonousSnake/src/index.ts
+++ GluttonousSnake/src/index.ts
@@ -16,16 +16,26 @@ const snake: Snake = new Snake();
 const scorePanel: ScorePanel = new ScorePanel();
 //定时器
 let interval: ReturnType<typeof setInterval>;
+let propInterval: ReturnType<typeof setInterval>;
 let propTimeOut: ReturnType<typeof setInterval>;
 //提示信息框
 let infos = document.getElementById("infos");
 //游戏结束时提示信息
 let msg = document.getElementById("msg");
 let tip = document.getElementById("tip");
+//
+let propInfos = document.getElementById("prop_infos");
+//
+let propTimes:number = 0;
 //调用定时器的频率，也就是移动的速度
 let moveSpeed:number = 300-(20*scorePanel.level);
-//吃到道具后的移动速度
-// let propMoveSpeed:number = 300-(20*scorePanel.level);
+
+
+let items = localStorage.getItem('snakeDatas');
+if(items){
+    let datas:[] = JSON.parse(items);
+    setRankInfos(datas)
+}
 
 /**
  * 游戏启动入口
@@ -66,6 +76,7 @@ function openInterval(speed:number){
                     clearInterval(interval);
                     openInterval(moveSpeed);
                 }, GameConstant.PROPS_ALIVE_TIME*1000)
+                setPropInfo('加速', GameConstant.PROPS_ALIVE_TIME);
             }
             //减速
             if(food.foodType === FoodType.SLOW_FOOD){
@@ -78,11 +89,29 @@ function openInterval(speed:number){
                     clearInterval(interval);
                     openInterval(moveSpeed);
                 }, GameConstant.PROPS_ALIVE_TIME*1000)
+                setPropInfo('减速', GameConstant.PROPS_ALIVE_TIME);
             }
             randomFood();
             snake.addNode(direction, scorePanel);
         }
     }, speed)
+}
+
+function setPropInfo(type:string, time:number){
+    if(propInterval){
+        clearInterval(propInterval);
+    }
+    propTimes = time;
+    propInfos.innerHTML = `<span>${type}时间：<strong>${time}</strong>秒</span>`
+    propInterval = setInterval(() => {
+        propTimes--;
+        if(propTimes < 0){
+            clearInterval(propInterval);
+            propInfos.innerHTML = ''
+        } else {
+            propInfos.innerHTML = `<span>${type}时间：<strong>${propTimes}</strong>秒</span>`
+        }
+    }, 1000);
 }
 
 /**
@@ -179,12 +208,73 @@ function restart(){
 function gameOver(){
     //移除定时器
     clearInterval(interval);
+    if(propTimeOut){
+        clearTimeout(propTimeOut)
+    }
+    if(propInterval){
+        clearInterval(propInterval)
+    }
     //移除按键监听
     document.removeEventListener("keydown", KeyDownHandler);
     document.addEventListener("keydown", KeyDownRestartHandler);
     
     infos.className = '';
+    propInfos.innerHTML = ''
+    
     msg.innerHTML = '游戏结束！分数：' + scorePanel.score;
     tip.innerHTML = '按空格键重新开始游戏';
+
+    const name = prompt('游戏结束.您可以输入您的名字记录分数');
+    if(!name){
+        return;
+    }
+    let snakeDatas = localStorage.getItem('snakeDatas');
+    if(snakeDatas){
+        let snakeDatasStr = localStorage.getItem('snakeDatas');
+        let snakeDatas =  JSON.parse(snakeDatasStr);
+        console.log(snakeDatas);
+        let exists:boolean = false;
+        for(let i = 0; i < snakeDatas; i++){
+            if(snakeDatas[i].name === name){
+                snakeDatas[i].score = scorePanel.score;
+                return;
+            }
+        }
+        if(!exists){
+            snakeDatas.push({
+                name:name,
+                score:scorePanel.score
+            })
+        }
+        localStorage.setItem('snakeDatas', JSON.stringify(snakeDatas));
+        setRankInfos(snakeDatas);
+    } else {
+        let data = [
+            {'name':name,'score':scorePanel.score}
+        ]
+        localStorage.setItem('snakeDatas', JSON.stringify(data))
+        setRankInfos(<[]>data);
+    }
+}
+
+function setRankInfos(datas:[]){
+    console.log(datas);
+    const rank = document.getElementById("rank");
+    let arr:string = '';
+    datas.sort((a,b)=>b['score']-a['score']);
+    for(let i = 0; i < datas.length; i++){
+        let name = datas[i]['name'];
+        let score = datas[i]['score'];
+        let fontSize:string;
+        if(i == 0){
+            fontSize = '24px'
+        } else if(i == 1){
+            fontSize = '18px'
+        } else if(i == 2){
+            fontSize = '16px'
+        }
+        arr += `<li class="rank_detail">第<strong style="font-size:${fontSize}">${i+1}</strong>名<span class="name">${name}</span><span class="score">${score}</span>分</li>`;
+    }
+    rank.innerHTML = arr
 }
 
